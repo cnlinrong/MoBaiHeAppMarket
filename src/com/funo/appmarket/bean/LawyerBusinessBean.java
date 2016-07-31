30e714ef9ea52e493771b8219807d02cbf6bbc90
genius-lin <718147913@qq.com>
2016-07-31 22:21:37 +0800

    OK

--- src/com/funo/appmarket/bean/LawyerBusinessBean.java
+++
@@ -1,7 +0,0 @@
-package com.funo.appmarket.bean;
-
-import com.funo.appmarket.bean.base.BasePagedDataBussniseBean;
-
-public class LawyerBusinessBean extends BasePagedDataBussniseBean<LawyerBean> {
-	
-}
--- src/org/evilbinary/tv/widget/BorderView.java
+++ src/org/evilbinary/tv/widget/BorderView.java
@@ -1,5 +1,7 @@
 package org.evilbinary.tv.widget;
 
+import android.annotation.SuppressLint;
+
 /**
  * 作者:evilbinary on 3/25/16.
  * 邮箱:rootdebug@163.com
@@ -14,164 +16,161 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 
-
 /**
- * 作者:evilbinary on 3/21/16.
- * 邮箱:rootdebug@163.com
+ * 作者:evilbinary on 3/21/16. 邮箱:rootdebug@163.com
  */
-public class BorderView<X extends View> implements ViewTreeObserver.OnGlobalFocusChangeListener, ViewTreeObserver.OnScrollChangedListener, ViewTreeObserver.OnGlobalLayoutListener, ViewTreeObserver.OnTouchModeChangeListener {
-    private String TAG = BorderView.class.getSimpleName();
+public class BorderView<X extends View>
+		implements ViewTreeObserver.OnGlobalFocusChangeListener, ViewTreeObserver.OnScrollChangedListener,
+		ViewTreeObserver.OnGlobalLayoutListener, ViewTreeObserver.OnTouchModeChangeListener {
 
-    private ViewGroup mViewGroup;
-    private Effect borderEffect;
+	private String TAG = BorderView.class.getSimpleName();
 
-    private X mView;
-    private View mLastView;
+	private ViewGroup mViewGroup;
+	private Effect borderEffect;
 
-    public interface Effect {
-        public void onFocusChanged(View target, View oldFocus, View newFocus);
+	private X mView;
+	private View mLastView;
 
-        public void onScrollChanged(View target, View attachView);
+	public interface Effect {
 
-        public void onLayout(View target, View attachView);
+		public void onFocusChanged(View target, View oldFocus, View newFocus);
 
-        public void onTouchModeChanged(View target, View attachView, boolean isInTouchMode);
+		public void onScrollChanged(View target, View attachView);
 
-        public void onAttach(View target, View attachView);
+		public void onLayout(View target, View attachView);
 
-        public void OnDetach(View targe, View view);
+		public void onTouchModeChanged(View target, View attachView, boolean isInTouchMode);
 
-        public <T> T toEffect(Class<T> t);
-    }
+		public void onAttach(View target, View attachView);
 
-    public BorderView(Context context) {
-        this(context, null, 0);
-    }
+		public void OnDetach(View targe, View view);
 
-    public BorderView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+		public <T> T toEffect(Class<T> t);
 
-    public BorderView(Context context, AttributeSet attrs, int defStyleAttr) {
-        init(context, attrs, defStyleAttr);
-    }
+	}
 
-    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        borderEffect = new BorderEffect();
-        mView = (X) new View(context, attrs, defStyleAttr);
-    }
+	public BorderView(Context context) {
+		this(context, null, 0);
+	}
 
-    public BorderView(X view) {
-        this.mView = view;
-        borderEffect = new BorderEffect();
-    }
-    public BorderView(X view,Effect effect) {
-        this.mView = view;
-        borderEffect =effect;
-    }
+	public BorderView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
 
-    public BorderView(Context context, int resId) {
-        this((X) LayoutInflater.from(context).inflate(resId, null, false));
-    }
+	public BorderView(Context context, AttributeSet attrs, int defStyleAttr) {
+		init(context, attrs, defStyleAttr);
+	}
 
-    public X getView() {
-        return mView;
-    }
+	private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+		borderEffect = new BorderEffect();
+		mView = (X) new View(context, attrs, defStyleAttr);
+	}
 
+	public BorderView(X view) {
+		this.mView = view;
+		borderEffect = new BorderEffect();
+	}
 
-    public void setBackgroundResource(int resId) {
-        if (mView != null)
-            mView.setBackgroundResource(resId);
-    }
+	public BorderView(X view, Effect effect) {
+		this.mView = view;
+		borderEffect = effect;
+	}
 
-    @Override
-    public void onScrollChanged() {
-        borderEffect.onScrollChanged(mView, mViewGroup);
-    }
+	public BorderView(Context context, int resId) {
+		this((X) LayoutInflater.from(context).inflate(resId, null, false));
+	}
 
-    @Override
-    public void onGlobalLayout() {
-        borderEffect.onLayout(mView, mViewGroup);
-    }
+	public X getView() {
+		return mView;
+	}
 
-    @Override
-    public void onTouchModeChanged(boolean isInTouchMode) {
-        borderEffect.onTouchModeChanged(mView, mViewGroup, isInTouchMode);
-    }
+	public void setBackgroundResource(int resId) {
+		if (mView != null)
+			mView.setBackgroundResource(resId);
+	}
 
-    @Override
-    public void onGlobalFocusChanged(View oldFocus, View newFocus) {
-        try {
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                if (oldFocus == null && mLastView != null) {
-                    oldFocus = mLastView;
-                }
-            }
-            if (borderEffect != null)
-                borderEffect.onFocusChanged(mView, oldFocus, newFocus);
-            mLastView = newFocus;
+	@Override
+	public void onScrollChanged() {
+		borderEffect.onScrollChanged(mView, mViewGroup);
+	}
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+	@SuppressLint("WrongCall")
+	@Override
+	public void onGlobalLayout() {
+		borderEffect.onLayout(mView, mViewGroup);
+	}
 
-    }
+	@Override
+	public void onTouchModeChanged(boolean isInTouchMode) {
+		borderEffect.onTouchModeChanged(mView, mViewGroup, isInTouchMode);
+	}
 
+	@Override
+	public void onGlobalFocusChanged(View oldFocus, View newFocus) {
+		try {
+			if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+				if (oldFocus == null && mLastView != null) {
+					oldFocus = mLastView;
+				}
+			}
+			if (borderEffect != null)
+				borderEffect.onFocusChanged(mView, oldFocus, newFocus);
+			mLastView = newFocus;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    public <T extends BorderEffect> T getEffect() {
-        return (T) borderEffect;
-    }
+	public <T extends BorderEffect> T getEffect() {
+		return (T) borderEffect;
+	}
 
-    public void setEffect(Effect borderEffect) {
-        this.borderEffect = borderEffect;
-    }
+	public void setEffect(Effect borderEffect) {
+		this.borderEffect = borderEffect;
+	}
 
+	public void attachTo(ViewGroup viewGroup) {
+		try {
+			if (viewGroup == null) {
+				if (mView.getContext() instanceof Activity) {
+					Activity activity = (Activity) mView.getContext();
+					viewGroup = (ViewGroup) activity.getWindow().getDecorView().getRootView();
+				}
+			}
+			if (mViewGroup != viewGroup) {
+				ViewTreeObserver viewTreeObserver = viewGroup.getViewTreeObserver();
+				if (viewTreeObserver.isAlive() && mViewGroup == null) {
+					viewTreeObserver.addOnGlobalFocusChangeListener(this);
+					viewTreeObserver.addOnScrollChangedListener(this);
+					viewTreeObserver.addOnGlobalLayoutListener(this);
+					viewTreeObserver.addOnTouchModeChangeListener(this);
+				}
+				mViewGroup = viewGroup;
+			}
+			borderEffect.onAttach(mView, viewGroup);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    public void attachTo(ViewGroup viewGroup) {
-        try {
-            if (viewGroup == null) {
-                if (mView.getContext() instanceof Activity) {
-                    Activity activity = (Activity) mView.getContext();
-                    viewGroup = (ViewGroup) activity.getWindow().getDecorView().getRootView();
-                }
-            }
+	public void detach() {
+		detachFrom(mViewGroup);
+	}
 
-            if (mViewGroup != viewGroup) {
-
-                ViewTreeObserver viewTreeObserver = viewGroup.getViewTreeObserver();
-                if (viewTreeObserver.isAlive() && mViewGroup == null) {
-                    viewTreeObserver.addOnGlobalFocusChangeListener(this);
-                    viewTreeObserver.addOnScrollChangedListener(this);
-                    viewTreeObserver.addOnGlobalLayoutListener(this);
-                    viewTreeObserver.addOnTouchModeChangeListener(this);
-                }
-                mViewGroup = viewGroup;
-            }
-            borderEffect.onAttach(mView, viewGroup);
-
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void detach() {
-        detachFrom(mViewGroup);
-    }
-
-    public void detachFrom(ViewGroup viewGroup) {
-        try {
-            if (viewGroup == mViewGroup) {
-                ViewTreeObserver viewTreeObserver = mViewGroup.getViewTreeObserver();
-                viewTreeObserver.removeOnGlobalFocusChangeListener(this);
-                viewTreeObserver.removeOnScrollChangedListener(this);
-                viewTreeObserver.removeOnGlobalLayoutListener(this);
-                viewTreeObserver.removeOnTouchModeChangeListener(this);
-                borderEffect.OnDetach(mView, viewGroup);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+	@SuppressLint("NewApi")
+	public void detachFrom(ViewGroup viewGroup) {
+		try {
+			if (viewGroup == mViewGroup) {
+				ViewTreeObserver viewTreeObserver = mViewGroup.getViewTreeObserver();
+				viewTreeObserver.removeOnGlobalFocusChangeListener(this);
+				viewTreeObserver.removeOnScrollChangedListener(this);
+				viewTreeObserver.removeOnGlobalLayoutListener(this);
+				viewTreeObserver.removeOnTouchModeChangeListener(this);
+				borderEffect.OnDetach(mView, viewGroup);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
